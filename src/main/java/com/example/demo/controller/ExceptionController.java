e1814133f96f95b61c41dbe0e536a9e5848e9a7b
Sekiguchi Kodai <k.sekiguchi@atue.jp>
2025-02-27 10:21:56 +0900

    更新:ExceptionController レスポンスステータス
    以下メソッドのレスポンスステータス 500 に変更
    handleDatabaseRegisterUserException
    handleException

--- src/main/java/com/example/demo/controller/ExceptionController.java
+++ src/main/java/com/example/demo/controller/ExceptionController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.example.demo.common.exception.DatabaseRegisterUserException;
 
 @ControllerAdvice
 public class ExceptionController {
 	
+	@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(exception = DatabaseRegisterUserException.class)
 	public String handleDatabaseRegisterUserException(DatabaseRegisterUserException exceprion) {
 		System.out.println(exceprion.getMessage());
@@ -15,6 +18,7 @@ public class ExceptionController {
 	}
 	
 	@ExceptionHandler(exception = Exception.class)
+	@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
 	public String handleException(Exception exceprion) {
 		System.out.println(exceprion.getMessage());
 		return "error_system";
