d7d1465c5a6f20dd6ac7f204e99250d14bd13030
Sekiguchi Kodai <k.sekiguchi@atue.jp>
2025-02-28 11:48:44 +0900

    更新:Controllerテストクラス 単体テスト化
    モックを使用してServiceクラスの実装に依存しないよう変更

--- src/test/java/com/example/demo/controller/FinishControllerTest.java
+++ src/test/java/com/example/demo/controller/FinishControllerTest.java
@@ -1,12 +1,18 @@
 package com.example.demo.controller;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Nested;
@@ -14,75 +20,90 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.FlashMap;
 
+import com.example.demo.entity.UserTable;
 import com.example.demo.model.User;
 import com.example.demo.model.UserAddress;
-import com.example.demo.test.dbunit.CsvDataSetLoader;
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.example.demo.service.UserService;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.github.springtestdbunit.annotation.DbUnitConfiguration;
-import com.github.springtestdbunit.annotation.ExpectedDatabase;
-import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 
-@TestExecutionListeners({
-	DependencyInjectionTestExecutionListener.class,
-	TransactionalTestExecutionListener.class,
-	DbUnitTestExecutionListener.class
-})
-@Transactional
+//@TestExecutionListeners({
+//	DependencyInjectionTestExecutionListener.class,
+//	TransactionalTestExecutionListener.class,
+//	DbUnitTestExecutionListener.class
+//})
+//@Transactional
 public class FinishControllerTest extends BaseControllerTest {
 	
+	private long initNo, initAge;
+	private String initName, initBirthday;
+	private User user;
+	
+	@MockitoBean
+	private UserService mockUserService;
+	
 	@Autowired
-	public FinishControllerTest(MockMvc mockMvc) {
+	public FinishControllerTest(MockMvc mockMvc, UserService userService) {
 		super(mockMvc);
+		this.mockUserService = userService;
 	}
 
+	@BeforeEach
+	public void initUser() {
+		initNo = 5L;
+		initName = "test5";
+		initAge = 50L;
+		initBirthday = "1975/01/01";
+		
+		user = new User();
+		user.setNo(initNo);
+		user.setName(initName);
+		user.setAge(initAge);
+		user.setBirthday(initBirthday);
+	}
+	
 	@Nested
 	@Order(1)
 	@TestMethodOrder(MethodOrderer.MethodName.class)
-	@DbUnitConfiguration(dataSetLoader = CsvDataSetLoader.class)
+//	@DbUnitConfiguration(dataSetLoader = CsvDataSetLoader.class)
 	class _P0101_commit {
 		
 		@Test
-		@DisplayName("正常_ユーザ情報がDBに登録されて画面がfinishに遷移")
-		@DatabaseSetup(value = "/com/example/demo/controller/finishcontroller/db/setup/")
-		@ExpectedDatabase(value = "/com/example/demo/controller/finishcontroller/db/commit/education.USER_TABLE.csv", assertionMode = DatabaseAssertionMode.NON_STRICT)
+		@DisplayName("正常_登録ユーザの情報をviewが受け取り画面がfinishに遷移する")
+//		@DatabaseSetup(value = "/com/example/demo/controller/finishcontroller/db/setup/")
+//		@ExpectedDatabase(value = "/com/example/demo/controller/finishcontroller/db/commit/education.USER_TABLE.csv", assertionMode = DatabaseAssertionMode.NON_STRICT)
 		public void _P010101_commit() throws Exception {
-			User user = new User();
-			user.setNo(4);
-			user.setName("test4");
-			user.setAge(40);
-			user.setBirthday("1985/01/01");
+			
+			UserTable ut = new UserTable();
+			ut.setNo(initNo);
+			ut.setName(initName);
+			ut.setAge(initAge);
+			ut.setBirthday(new Date(new SimpleDateFormat("yyyy/MM/dd").parse(initBirthday).getTime()));
+			
+			doReturn(ut).when(mockUserService).register(user);
 			
 			mockMvc.perform(post("/commit").param("commit", "commit").flashAttr("user", user))
 				   .andDo(print(fileWriter))
 				   .andExpect(status().isOk())
-				   .andExpect(view().name("user/register/finish"));
+				   .andExpect(view().name("user/register/finish"))
+				   .andExpect(model().attribute("userTable", hasProperty("name", is(initName))));
 		}
 	}
 	
 	@Nested
 	@Order(2)
 	@TestMethodOrder(MethodOrderer.MethodName.class)
-	@DbUnitConfiguration(dataSetLoader = CsvDataSetLoader.class)
+//	@DbUnitConfiguration(dataSetLoader = CsvDataSetLoader.class)
 	class _P0201_back {
 		
 		@Test
 		@DisplayName("正常_form画面にリダイレクトしUserオブジェクトをViewに渡す")
 		@DatabaseSetup(value = "/com/example/demo/controller/finishcontroller/db/setup/")
 		public void _P020101_back() throws Exception {
-			User user = new User();
-			user.setNo(4);
-			user.setName("test4");
-			user.setAge(40);
-			user.setBirthday("1985/01/01");
 			
 			UserAddress ua = new UserAddress();
 			ua.setPrefecture("北海道");
@@ -99,7 +120,7 @@ public class FinishControllerTest extends BaseControllerTest {
 			FlashMap flashMap = mvcResult.getFlashMap(); // 引き継ぎパラメータを取得
 			User actual = (User) flashMap.get("user");
 			
-			assertThat(actual.getName()).isEqualTo("test4");
+			assertThat(actual.getName()).isEqualTo(initName);
 		}
 	}
 	
--- src/test/java/com/example/demo/controller/HomeControllerTest.java
+++ src/test/java/com/example/demo/controller/HomeControllerTest.java
@@ -1,13 +1,10 @@
 package com.example.demo.controller;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.util.Arrays;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
@@ -16,63 +13,40 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.model.User;
-import com.example.demo.model.UserAddress;
-import com.example.demo.test.dbunit.CsvDataSetLoader;
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.example.demo.service.UserService;
 
-@TestExecutionListeners({
-	DependencyInjectionTestExecutionListener.class,
-	TransactionalTestExecutionListener.class,
-	DbUnitTestExecutionListener.class
-})
-@Transactional
 public class HomeControllerTest extends BaseControllerTest {
 
+	@MockitoBean
+	private UserService mockUserService;
+	
 	@Autowired
-	public HomeControllerTest(MockMvc mockMvc) {
+	public HomeControllerTest(MockMvc mockMvc, UserService userService) {
 		super(mockMvc);
+		this.mockUserService = userService;
 	}
 	
 	@Nested
 	@Order(1)
 	@TestMethodOrder(MethodOrderer.MethodName.class)
-	@DbUnitConfiguration(dataSetLoader = CsvDataSetLoader.class)
 	class _G0101_readForm {
 		
 		@Test
-		@DisplayName("正常_画面がformに遷移")
+		@DisplayName("正常_User-Noを取得したUserオブジェクトをViewが受け取り画面がformに遷移する")
 		public void _G010101_readForm() throws Exception {
-			mockMvc.perform(get("/form"))
-				   .andDo(print(fileWriter))
-				   .andExpect(status().isOk())
-				   .andExpect(view().name("user/register/form"));
-		}
-		
-		@Test
-		@DisplayName("正常_UserオブジェクトをViewに渡す")
-		public void _G010102_readForm() throws Exception {
-			mockMvc.perform(get("/form"))
-			 	   .andDo(print(fileWriter))
-				   .andExpect(model().attribute("user", instanceOf(User.class)));
-		}
-		
-		@Test
-		@DisplayName("正常_採番したUser-NoをViewに渡す")
-		@DatabaseSetup("/com/example/demo/controller/homecontroller/db/setup/")
-		public void _G010103_readForm() throws Exception {
-			long expectedNo = 4L;
+			long expectedNo = 5L;
+			
+			int mockResturnNo = (int) expectedNo;
+			doReturn(mockResturnNo).when(mockUserService).getNewNo();
 			
 			mockMvc.perform(get("/form"))
 				   .andDo(print(fileWriter))
+				   .andExpect(status().isOk())
+				   .andExpect(view().name("user/register/form"))
 				   .andExpect(model().attribute("user", hasProperty("no", is(expectedNo))));
 		}
 	}
@@ -80,46 +54,39 @@ public class HomeControllerTest extends BaseControllerTest {
 	@Nested
 	@Order(2)
 	@TestMethodOrder(MethodOrderer.MethodName.class)
-	@DbUnitConfiguration(dataSetLoader = CsvDataSetLoader.class)
 	class _P0101_confirm {
 		
 		@Test
-		@DisplayName("正常_画面がconfirmに遷移")
-		@DatabaseSetup("/com/example/demo/controller/homecontroller/db/setup/")
+		@DisplayName("正常_ユーザ情報をViewが受け取り画面がconfirmに遷移")
 		public void _P010101_confirm() throws Exception {
 			User user = new User();
-			user.setNo(4);
+			user.setNo(4L);
 			user.setName("test-user");
-			user.setAge(10);
-			user.setBirthday("2015/01/01"); 
-			
-			UserAddress ua = new UserAddress();
-			ua.setPrefecture("北海道");
-			ua.setCity("函館市");
-			ua.setAddressLine(Arrays.asList("33","4","6"));
-			user.setUserAddress(ua);
+			user.setAge(10L);
+			user.setBirthday("2015/01/01");
 			
 			// flashAttr() Postメソッドのリクエストパラメータにオブジェクトを渡す場合
 			mockMvc.perform(post("/form").flashAttr("user", user))
 				   .andDo(print(fileWriter))
 				   .andExpect(status().isOk())
-				   .andExpect(view().name("user/register/confirm"));
+				   .andExpect(view().name("user/register/confirm"))
+				   .andExpect(model().attribute("user", sameInstance(user)));
 		}
 		
 		@Test
 		@DisplayName("正常_フォーム年齢欄の入力値が0未満の場合画面がirregularに遷移")
-		@DatabaseSetup("/com/example/demo/controller/homecontroller/db/setup/")
 		public void _P010102_confirm() throws Exception {
 			User user = new User();
-			user.setNo(4);
+			user.setNo(4L);
 			user.setName("test-user");
-			user.setAge(-1);
+			user.setAge(-1L);
 			user.setBirthday("2015/01/01");
 			
 			mockMvc.perform(post("/form").flashAttr("user", user))
 				   .andDo(print(fileWriter))
 				   .andExpect(status().isOk())
-				   .andExpect(view().name("user/register/irregular"));;
+				   .andExpect(view().name("user/register/irregular"))
+				   .andExpect(model().attribute("user", hasProperty("errMsg", is("年齢の値が不正です。"))));
 		}
 	}
 }
